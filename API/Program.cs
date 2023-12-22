60e344ed53bf710d2fd58070e6f8eb9ff858617e
VM <vcmuner@gmail.com>
2023-12-22 20:58:19 -0300

    Adding CORS and changing application port

--- API/Program.cs
+++ API/Program.cs
@@ -13,7 +13,7 @@ builder.Services.AddDbContext<StoreContext>(opt =>
 {
 	opt.UseSqlite(builder.Configuration.GetConnectionString("Default"));
 });
-
+builder.Services.AddCors();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -22,7 +22,10 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
+//Middleware
+app.UseCors (opt => {
+	opt.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:3000");
+});
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
